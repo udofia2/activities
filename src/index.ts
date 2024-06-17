9870e04a230574d67eb48fa911422a9145d7c6bf
Abasiodiong Udofia <enalsy22@gmail.com>
2024-06-17 10:32:26 +0100

    feat(start): start of project

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/config';
+import logger from './modules/logger/logger';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+let server: any = createServer(app);
+export const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  logger.info('A client connected');
+  socket.emit('user just got connected');
+});
+
+mongoose.connect(config.mongoose.url).then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port}`);
+  });
+  
+  io.listen(server)
+});
+
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error: string) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
